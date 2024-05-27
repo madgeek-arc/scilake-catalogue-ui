2c7d4d82f767bfa77efb65a150f477a5c53e4f90
jBalasis <j.balasis92@gmail.com>
2024-05-27 14:06:50 +0300

    added routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,16 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+    {
+      path: '',
+      loadChildren: () => import('../dataSpaceUI/app/dataSpaceUI.module').then(m => m.DataSpaceUIModule)
+    },
+    {
+      path: 'requests',
+      loadChildren: () => import('../requests-ui/requests.module').then(m => m.RequestsModule)
+    }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/requests-ui/requests/create/create.request.component.ts
+++ src/requests-ui/requests/create/create.request.component.ts
@@ -20,7 +20,7 @@ import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {From, Ticket} from "../../entities/ticket";
 import {RequestsService} from "../../services/requests.service";
 import {UserService} from "../../../dataSpaceUI/app/services/user.service";
-import * as ClassicEditor from '@ckeditor/ckeditor5-build-classic';
+import ClassicEditor from '@ckeditor/ckeditor5-build-classic';
 import UIkit from "uikit";
 
 @Component({
